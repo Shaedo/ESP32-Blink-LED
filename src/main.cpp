a92035fc9a40d7ea3de27c44533c3866e51ea3e2
Justin Ford <shaedomail@gmail.com>
2025-01-02 06:16:44 +1100

    Test Project Created

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+
+#define LED 35
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED, HIGH);
+  Serial.print("LED is on - ");
+  Serial.println(LED);
+  delay(1000);
+  digitalWrite(LED, LOW);
+  Serial.println("LED is off");
+  delay(1000);
+}
